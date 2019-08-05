ba2a10090796bfed0af5d1ff255c644de02dd907
Deakhadevi <50576902+Deakhadevi@users.noreply.github.com>
2019-08-05 11:03:19 +0530

    Updated HTML Parser (#8)
    Filter text and store in a filee

--- HtmlParserTokenize.py
+++ HtmlParserTokenize.py
@@ -66,3 +66,45 @@ text = soup.get_text(strip=True)
 tokens = [t for t in text.split()] 
 #print (tokens)
 
+# In [10]
+### Convert that text into tokens and Count Word Frequency by FreqDist
+#### Written  on 31-Jul-2019
+
+from bs4 import BeautifulSoup
+import urllib.request
+import nltk 
+response = urllib.request.urlopen('file:///C:/reut2-002.html') 
+html = response.read() 
+soup = BeautifulSoup(html,"html5lib") 
+text = soup.get_text(strip=True) 
+tokens = [t for t in text.split()] 
+freq = nltk.FreqDist(tokens) 
+for key,val in freq.items(): 
+ print (str(key) + ':' + str(val))
+
+
+# In [10]
+### Convert that text into tokens and filter the text , Filtered text storing in a file.
+from bs4 import BeautifulSoup
+import urllib.request
+import nltk 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+# IO library used for Read and write a file
+import io 
+
+#word_tokenize accepts a string as an input, not a file. 
+stop_words = set(stopwords.words('english')) 
+
+response = urllib.request.urlopen('file:///C:/reut2-002.html') 
+html = response.read() 
+soup = BeautifulSoup(html,"html5lib") 
+
+text = soup.get_text(strip=True) 
+words=word_tokenize(text)
+for w in words:
+        if not w in stop_words: 
+            appendFile = open('e:/filteredtext.txt','a') 
+            appendFile.write(" "+w) 
+            appendFile.close() 
+
